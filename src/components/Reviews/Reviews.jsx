9e9c10ce57e7850505c0aa66e081d4356ea60e66
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-12-31 19:34:39 +0200

    2

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -12,15 +12,21 @@ const Reviews = () => {
     getMovieReviews(movieId).then(review => setReviews(review));
   }, [movieId]);
 
+  console.log(reviews);
+
   return (
     <ReviewsList>
-      {reviews.map(({ author, content, id }) => {
-        return (
-          <li key={id}>
-            <Review author={author} content={content} />
-          </li>
-        );
-      })}
+      {reviews.length > 0 ? (
+        reviews.map(({ author, content, id }) => {
+          return (
+            <li key={id}>
+              <Review author={author} content={content} />
+            </li>
+          );
+        })
+      ) : (
+        <p>No reviews present!</p>
+      )}
     </ReviewsList>
   );
 };
